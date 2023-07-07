3de7ace8a0e700246dc8a042baa54d7ecbf19fc3
etandmouse <etandmouse@gmail.copm>
2023-07-07 22:14:43 +0800

    Init the project.

--- game-modifier.cpp
+++ game-modifier.cpp
@@ -7,4 +7,45 @@ Description:
 	4. Exit
 Build by: etandmouse@gmail.com
 */
+#include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <TlHelp32.h>
 
+int main(void)
+{
+	//input for user
+	int select = 0;
+	while (true)
+	{
+		//display menu
+		ShowMenu();
+
+		while (!scanf_s("%d", &select))
+		{
+			rewind(stdin);
+		}
+
+		switch (select)
+		{
+		case 1:
+			ShowProcessList();
+			break;
+		case 2:
+			EditProcessData();
+			break;
+		case 3:
+			KillProcess();
+			break;
+		case 4:
+			printf("Thank for your using....");
+			system("pause");
+			return 0;
+		default:
+			printf("Please select again 1~4");
+			break;
+		}
+	}	
+
+	return 0;
+}
